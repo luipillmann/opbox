6bb5e9a7c564e9b9d0ca64e964db35ccdcfacc90
Lui <lui.pillmann@gmail.com>
2015-11-01 19:26:19 -0200

    converting time values, problem with float conversion...

--- raspi/SerialRead_2.py
+++ raspi/SerialRead_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# Serial communication code on Peter Gibson's (http://stackoverflow.com/questions/24074914/python-to-arduino-serial-read-write)
+
 import serial
 import syslog
 import time
@@ -13,12 +15,14 @@ TIME_HEADER = 'T'        # Header tag for serial time sync message
 
 #The following line is for serial over GPIO
 port = '/dev/tty.usbmodem1411' # note I'm using Mac OS-X
+#port = '/dev/ttyACM0' # Raspberry Pi's port
+
 
 ard = serial.Serial(port,9600,timeout=5)
 time.sleep(2) # wait for Arduino
 
-test_file = "test.txt"
-
+test_file = 'test.txt'
+start_time = ''
 
 def printSerial():
 	# Serial read section
@@ -32,7 +36,7 @@ def writeSerial(txt):
 	# Serial write section
     ardCmd = txt;
     ard.flush()
-    print ("Python value sent: ")
+    print ('Python value sent: ')
     print (txt)
     ard.write(txt)
     time.sleep(1) # I shortened this to match the new value in your Arduino code
@@ -49,25 +53,24 @@ def readFile(textfile):
 
 def parseFile(textfile):
 	line_measures = []
+	line_logs = []
 	with open(textfile) as f:
-		content = f.readlines()
-		for line in content:
-			if line[0] == STARTTIME_HEADER:
+		content = f.readlines() 
+		for line in content:	# iterates through lines and checks header
+			if line[0] == LOG_HEADER:
+				line_logs.append(line[1:])
+			elif line[0] == STARTTIME_HEADER:
 				start_time = line[1:]
+				start_time = start_time[:4] #
+				print '\n\nStart time: '
+				print start_time
 			elif line[0] == MEASUREMENT_HEADER:
 				line_measures.append(line[1:])
-
-	print "\n\nStart time: " + start_time;
-	print "\n\nData extracted"
-	print line_measures
 
 	pairs = []
 
 	for line in line_measures:
 		[pairs.append(x.strip()) for x in line.split(';')]
-	
-	print "\n\nPairs of data"
-	print pairs
 
 	m_times = []
 	m_values = []
@@ -77,16 +80,17 @@ def parseFile(textfile):
 		m_times.append(aux[0])
 		m_values.append(aux[2])
 
-		#[m_times.append(x.strip()) for x in item.partition(',')[]]
-		#[m_values.append(x.strip()) for x in item.rsplit(',',1)]
-
-	print "\n\nTime and values"
+	print '\n\nTime and values'
 	print m_times
 	print m_values
-	print len(m_values) == len(m_times)
 	plotChart(m_times, m_values)
 
 def plotChart(x_values, y_values):
+	
+	start_t = float(start_time)
+	x_values = [(float(x)-start_t)/1000 for x in x_values] # converts values to float and for X, removes offset and divides by 1000 (data in ms)
+	y_values = [float(y) for y in y_values]
+
 	trace0 = go.Scatter(
 	    x=x_values,
 	    y=y_values,
@@ -103,7 +107,7 @@ writeSerial(user_txt)
 
 i = 0
 
-while (i < 1000):
+while (i < 100):
 	msg = printSerial()
 	if (msg[0] == LOG_HEADER) or (msg[0] == STARTTIME_HEADER) or (msg[0] == MEASUREMENT_HEADER):
 		writeFile(test_file, msg)
